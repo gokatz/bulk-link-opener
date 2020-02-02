80e6ab20bea013f19fcdff18bdd1aec28a1329d3
Gokul Kathirvel <gokulkathirvel@live.com>
2020-02-03 00:26:58 +0530

    alignments

--- src/Item.js
+++ src/Item.js
@@ -17,47 +17,56 @@ export default class Item extends React.Component {
 
   render() {
     let { text = {}, deleteItem, starItem, unstarItem } = this.props;
-    let { id, star, value, date, dateStr } = text;
+    let { id, star, value } = text;
 
     let dateString = this.computeDate(text);
 
     return (
-      <div className="form-check">
-        <label className="form-check-label link-content" htmlFor={id}>
-          <input
-            className="form-check-input"
-            type="checkbox"
-            onChange={event => {
-              text.isSelected = event.target.checked;
-            }}
-            id={id}
-          />
-          {star ? "⭐" : null} &nbsp;
-          <em class="text-muted italics"> {dateString} &nbsp; </em>
-          {value} &nbsp;&nbsp;
-        </label>
-        {star ? (
-          <small
-            className="btn-link cursor-pointer"
-            onClick={() => unstarItem(id)}
-          >
-            (UnStar)
-          </small>
-        ) : (
-          <small
-            className="btn-link cursor-pointer"
-            onClick={() => starItem(id)}
-          >
-            (Star)
-          </small>
-        )}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <small
-          className="btn-link cursor-pointer"
-          onClick={() => deleteItem(text)}
-        >
-          (Del)
-        </small>
+      <div className="">
+        <div className="row">
+          <div className="col-9">
+            <div className="form-check">
+              <label className="form-check-label link-content" htmlFor={id}>
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  onChange={event => {
+                    text.isSelected = event.target.checked;
+                  }}
+                  id={id}
+                />
+                {star ? "⭐ " : null}
+                <em className="text-muted italics"> {dateString} &nbsp; </em>
+                {value} &nbsp;&nbsp;
+              </label>
+            </div>
+          </div>
+          <div className="col-2">
+            {star ? (
+              <small
+                className="btn-link cursor-pointer"
+                onClick={() => unstarItem(id)}
+              >
+                (UnStar)
+              </small>
+            ) : (
+              <small
+                className="btn-link cursor-pointer"
+                onClick={() => starItem(id)}
+              >
+                (Star)
+              </small>
+            )}
+          </div>
+          <div className="col-1">
+            <small
+              className="btn-link cursor-pointer"
+              onClick={() => deleteItem(text)}
+            >
+              (Del)
+            </small>
+          </div>
+        </div>
       </div>
     );
   }
